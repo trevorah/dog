460ba6cecdc168910939a9b92c8e1adeda9e0c4d
Andy Trevorah <a.trevorah@gmail.com>
2018-10-29 18:33:40 +0000

    intial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module dog
+
+require (
+	golang.org/x/crypto v0.0.0-20181025213731-e84da0312774
+	golang.org/x/image v0.0.0-20181025233408-2a2258ff36e9
+	golang.org/x/sys v0.0.0-20181026203630-95b1ffbd15a5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"math"
+	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/image/draw"
+)
+
+func main() {
+	infile, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer infile.Close()
+
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		panic(err)
+	}
+
+	terminalWidth, terminalHeight, err := terminal.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		panic(err)
+	}
+
+	imgWidth := src.Bounds().Max.X
+	imgHeight := src.Bounds().Max.Y
+
+	scale := math.Min(float64(terminalWidth)/float64(imgWidth), float64(terminalHeight*2)/float64(imgHeight))
+
+	rect := image.Rect(0, 0, int(float64(imgWidth)*scale), int(float64(imgHeight)*scale))
+
+	img := image.NewRGBA(rect)
+	draw.NearestNeighbor.Scale(img, rect, src, src.Bounds(), draw.Over, nil)
+
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y += 2 {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			r1, g1, b1, _ := simplify(img.At(x, y).RGBA())
+			r2, g2, b2, _ := simplify(img.At(x, y+1).RGBA())
+
+			fmt.Printf("\x1b[48;2;%d;%d;%dm\x1b[38;2;%d;%d;%dm▄\x1b[0m", r1, g1, b1, r2, g2, b2)
+		}
+		fmt.Print("\n")
+	}
+}
+
+func simplify(bigR, bigG, bigB, bigA uint32) (r, g, b, a uint8) {
+	return uint8(bigR / 0x101), uint8(bigG / 0x101), uint8(bigB / 0x101), uint8(bigA / 0x101)
+}
